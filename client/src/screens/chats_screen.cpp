31971ee16b62d66bcbe01c47500ffbd1fba72c24
ra1zzzengpt <ra1zzzengpt@gmail.com>
2026-05-06 23:20:04 +0300

    syntax + architecture changes

--- client/src/screens/chats_screen.cpp
+++ client/src/screens/chats_screen.cpp
@@ -4,27 +4,52 @@
 
 #include <string>
 
-namespace screen {
+namespace
+{
+    void chats_print(const std::vector<ChatInfo>& chats)
+    {
+        for (std::size_t i = 0; i != chats.size(); ++i)
+        {
+            io::print("[" + std::to_string(i + 1) + "] - "
+                + chats[i].peer_nick + "(" + std::to_string(chats[i].peer_id) + ")");
+        }
+    }
+
+    void add_chat()
+    {
+
+    }
+}
+
+namespace screen
+{
     ChatsScreen::ChatsScreen(AppConfig& cfg) : config_(cfg) {}
 
-    void ChatsScreen::run() {
+    void ChatsScreen::run()
+    {
         bool running = true;
         while (running) {
             printScreen();
-            switch (io::ScanUint32("> ")) {
-                case static_cast<uint32_t>(kCHATS_MENU::kSelectChat): {
+            chats_print(config_.chats);
+            switch (io::ScanUint32("> "))
+            {
+                case static_cast<uint32_t>(kCHATS_MENU::kSelectChat):
+                {
                     // todo select chat
                     break;
                 }
-                case static_cast<uint32_t>(kCHATS_MENU::kAddChat): {
+                case static_cast<uint32_t>(kCHATS_MENU::kAddChat):
+                {
                     // todo add new chat
                     break;
                 }
-                case static_cast<uint32_t>(kCHATS_MENU::kExit): {
+                case static_cast<uint32_t>(kCHATS_MENU::kExit):
+                {
                     running = false;
                     break;
                 }
-                default: {
+                default:
+                {
                     io::print("[Error]: Enter value from " + std::to_string(static_cast<int>(kCHATS_MENU::kMinChoice)) +
                         " to " + std::to_string(static_cast<int>(kCHATS_MENU::kMaxChoice)), io::COLOR::RED);
                 }
@@ -32,7 +57,8 @@ namespace screen {
         }
     }
 
-    void ChatsScreen::printScreen() {
+    void ChatsScreen::printScreen()
+    {
         io::print("Chats Screen");
         io::print("1 - Select chat");
         io::print("2 - Add new chat");
