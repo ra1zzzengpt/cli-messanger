76c28783773bda95b04164b1729fbc0e90f4c2f0
ra1zzzengpt <ra1zzzengpt@gmail.com>
2026-05-06 23:20:41 +0300

    main.cpp changes

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,7 +1,20 @@
-#include "app/app.h"
+#include "app/app_controller.h"
+#include "screens/main_screen.h"
+#include "utils/files/config_storage/config_storage.h"
+#include "api/message_api/fakeapi/fake_message_api.h"
+namespace
+{
+    // YOU CAN CHANGE IT ON YOUR CONFIG FILEPATH
+    constexpr auto kConfigPath = "client/assets/save/save.json";
+}
 
 int main()
 {
-    Application app;
-    app.Run();
+
+    AppController controller(std::make_unique<api::FakeMessageApi>(),std::make_unique<utils::ConfigStorage>(kConfigPath));
+
+    screen::MainScreen mainMenu(controller);
+    mainMenu.Run();
+
+    return 0;
 }
