eb02500cd0da8cd9ef1453ea4c276fd4bacd0ea2
ra1zzzengpt <ra1zzzengpt@gmail.com>
2026-05-06 23:19:11 +0300

    syntax + architecture changes

--- client/src/screens/server_screen.cpp
+++ client/src/screens/server_screen.cpp
@@ -6,29 +6,37 @@
 
 #include "utils/files/file_utils.h"
 
-namespace screen {
-    ServerScreen::ServerScreen(AppConfig& cfg) : config_(cfg) {}
+namespace screen
+{
+    ServerScreen::ServerScreen(AppConfig& cfg) : config_(cfg)
+    { }
 
-    void ServerScreen::run() {
+    void ServerScreen::run()
+    {
         bool running = true;
         while (running) {
             printScreen();
-            switch (io::ScanUint32("> ")) {
-                case static_cast<uint32_t>(kSERVER_MENU::kChangeIp): {
+            switch (io::ScanUint32("> "))
+            {
+                case static_cast<uint32_t>(kSERVER_MENU::kChangeIp):
+                {
                     config_.server.host = io::ScanString("Enter new host: ");
                     utils::SaveConfig(config_);
                     break;
                 }
-                case static_cast<uint32_t>(kSERVER_MENU::kChangePort): {
+                case static_cast<uint32_t>(kSERVER_MENU::kChangePort):
+                {
                     config_.server.port = io::ScanUint32("Enter new port: ");
                     utils::SaveConfig(config_);
                     break;
                 }
-                case static_cast<uint32_t>(kSERVER_MENU::kExit): {
+                case static_cast<uint32_t>(kSERVER_MENU::kExit):
+                {
                     running = false;
                     break;
                 }
-                default: {
+                default:
+                {
                     io::print("[Error]: Enter value from " + std::to_string(static_cast<int>(kSERVER_MENU::kMinChoice)) +
                         " to " + std::to_string(static_cast<int>(kSERVER_MENU::kMaxChoice)), io::COLOR::RED);
                 }
@@ -36,7 +44,8 @@ namespace screen {
         }
     }
 
-    void ServerScreen::printScreen() {
+    void ServerScreen::printScreen()
+    {
         io::print("Server Screen");
         io::print("Current server:");
         io::print("Host: " + config_.server.host);
