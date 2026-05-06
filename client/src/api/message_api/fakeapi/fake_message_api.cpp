5addaf450620e0b24d66e3657aafe9c344bd70cc
ra1zzzengpt <ra1zzzengpt@gmail.com>
2026-05-06 23:21:18 +0300

    fake api for testing

--- client/src/api/message_api/fakeapi/fake_message_api.cpp
+++ client/src/api/message_api/fakeapi/fake_message_api.cpp
@@ -1,5 +1,35 @@
-//
-// Created by devnull on 05.05.2026.
-//
-
 #include "fake_message_api.h"
+
+bool api::FakeMessageApi::registerUser(std::uint64_t id, const std::string &nick)
+{
+    return true;
+}
+
+std::optional<UserInfo> api::FakeMessageApi::getUsernameById(const std::uint64_t id)
+{
+    return UserInfo{id,"FakeUser"};
+}
+
+bool api::FakeMessageApi::updateNickname(std::uint64_t id, const std::string &newNick)
+{
+    return true;
+}
+
+bool api::FakeMessageApi::sendMessage(std::uint64_t fromId, std::uint64_t toId, const std::string &text)
+{
+    return true;
+}
+
+std::vector<Message> api::FakeMessageApi::fetchMessages(const std::uint64_t myId, const std::uint64_t peerId, const uint64_t sinceMessageId)
+{
+    Message message;
+    message.id = sinceMessageId;
+    message.from_id = peerId;
+    message.from_nick = "FakeUser";
+    message.to_id = myId;
+    message.text = "text";
+    message.created_at = "[2026-04-05]";
+    std::vector<Message> messages;
+    messages.push_back(message);
+    return messages;
+}
--- client/src/api/message_api/fakeapi/fake_message_api.h
+++ client/src/api/message_api/fakeapi/fake_message_api.h
@@ -1,5 +1,41 @@
 #pragma once
+#include <optional>
+#include <cstdint>
+#include <string>
+
+#include "api/message_api/imessage_api.h"
+#include "models/user_info.h"
+#include "models/message.h"
 
 namespace api {
+    class FakeMessageApi final : public IMessageApi{
+    public:
+        FakeMessageApi() = default;
 
+        bool registerUser(
+            std::uint64_t id,
+            const std::string& nick
+        ) override;
+
+        std::optional<UserInfo> getUsernameById(
+            std::uint64_t id
+        ) override;
+
+        bool updateNickname(
+            std::uint64_t id,
+            const std::string& newNick
+        ) override;
+
+        bool sendMessage(
+            std::uint64_t fromId,
+            std::uint64_t toId,
+            const std::string& text
+        ) override;
+
+        std::vector<Message> fetchMessages(
+            std::uint64_t myId,
+            std::uint64_t peerId,
+            uint64_t sinceMessageId
+        ) override;
+    };
 }
