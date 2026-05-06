164a359f47f76d1a1c2f3fcff60b42b26cfa0da6
ra1zzzengpt <ra1zzzengpt@gmail.com>
2026-05-06 23:19:36 +0300

    syntax + architecture changes

--- client/src/screens/profile_screen.cpp
+++ client/src/screens/profile_screen.cpp
@@ -2,24 +2,33 @@
 #include "utils/console/console.h"
 #include "utils/files/file_utils.h"
 
-namespace screen {
-    ProfileScreen::ProfileScreen(AppConfig& config) : config_(config) {}
+namespace screen
+{
+    ProfileScreen::ProfileScreen(AppController& controller) : controller_(controller)
+    { }
 
-    void ProfileScreen::run() {
+    void ProfileScreen::run()
+    {
         bool running = true;
-        while (running) {
+        while (running)
+        {
             printScreen();
-            switch (io::ScanUint32("> ")) {
-                case static_cast<uint32_t>(kPROFILE_MENU::kChangeNickname): {
-                    config_.user.nick = io::ScanString("Enter new nickname: ");
+            switch (io::ScanUint32("> "))
+            {
+                case static_cast<uint32_t>(kPROFILE_MENU::kChangeNickname):
+                {
+                    config_.user.nickname = io::ScanString("Enter new nickname: ");
+                    if ()
                     utils::SaveConfig(config_);
                     break;
                 }
-                case static_cast<uint32_t>(kPROFILE_MENU::kExit): {
+                case static_cast<uint32_t>(kPROFILE_MENU::kExit):
+                {
                     running = false;
                     break;
                 }
-                default: {
+                default:
+                {
                     io::print("[Error]: Enter value from " + std::to_string(static_cast<int>(kPROFILE_MENU::kMinChoice)) +
                         " to " + std::to_string(static_cast<int>(kPROFILE_MENU::kMaxChoice)), io::COLOR::RED);
                 }
@@ -27,10 +36,11 @@ namespace screen {
         }
     }
 
-    void ProfileScreen::printScreen() {
+    void ProfileScreen::printScreen()
+    {
         io::print("Profile Screen");
         io::print("Your profile:");
-        io::print("Nickname: " + config_.user.nick);
+        io::print("Nickname: " + config_.user.nickname);
         io::print("Id: " + std::to_string(config_.user.id));
         io::print("1 - Change nickname");
         io::print("2 - Exit");
