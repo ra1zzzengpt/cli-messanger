8c6a74c77a77963b015320f27e421aba73b87939
devnull <ra1zzzengpt@gmail.com>
2026-05-07 19:55:18 +0300

    fake api rework for tests

--- client/src/api/message_api/fakeapi/fake_message_api.cpp
+++ client/src/api/message_api/fakeapi/fake_message_api.cpp
@@ -1,4 +1,17 @@
 #include "fake_message_api.h"
+#include <chrono>
+#include <iomanip>
+#include <sstream>
+
+namespace {
+    std::string getCurrentTimestamp() {
+        const auto now = std::chrono::system_clock::now();
+        const auto time = std::chrono::system_clock::to_time_t(now);
+        std::stringstream ss;
+        ss << std::put_time(std::localtime(&time), "%H:%M:%S");
+        return ss.str();
+    }
+}
 
 bool api::FakeMessageApi::registerUser(std::uint64_t id, const std::string &nick)
 {
@@ -7,7 +20,7 @@ bool api::FakeMessageApi::registerUser(std::uint64_t id, const std::string &nick
 
 std::optional<UserInfo> api::FakeMessageApi::getUsernameById(const std::uint64_t id)
 {
-    return UserInfo{id,"FakeUser"};
+    return UserInfo{id, "FakeUser"};
 }
 
 bool api::FakeMessageApi::updateNickname(std::uint64_t id, const std::string &newNick)
@@ -22,14 +35,27 @@ bool api::FakeMessageApi::sendMessage(std::uint64_t fromId, std::uint64_t toId, 
 
 std::vector<Message> api::FakeMessageApi::fetchMessages(const std::uint64_t myId, const std::uint64_t peerId, const uint64_t sinceMessageId)
 {
-    Message message;
-    message.id = sinceMessageId;
-    message.from_id = peerId;
-    message.from_nick = "FakeUser";
-    message.to_id = myId;
-    message.text = "text";
-    message.created_at = "[2026-04-05]";
     std::vector<Message> messages;
-    messages.push_back(message);
+
+    if (sinceMessageId > 0) {
+        Message message;
+        message.id = static_cast<uint32_t>(sinceMessageId + 1);
+        message.from_id = peerId;
+        message.from_nick = "FakeUser";
+        message.to_id = myId;
+        message.text = "Response to your message (ID: " + std::to_string(sinceMessageId) + ")";
+        message.created_at = getCurrentTimestamp();
+        messages.push_back(message);
+    } else {
+        Message welcome;
+        welcome.id = 1;
+        welcome.from_id = peerId;
+        welcome.from_nick = "FakeUser";
+        welcome.to_id = myId;
+        welcome.text = "Hello! I am a fake user.";
+        welcome.created_at = getCurrentTimestamp();
+        messages.push_back(welcome);
+    }
+    
     return messages;
 }
