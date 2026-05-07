689e30b0ef3af970c91e457484947c0ba0ecb21f
devnull <ra1zzzengpt@gmail.com>
2026-05-07 19:51:59 +0300

    chat screen done with little changes in class

--- client/src/screens/chat_screen.cpp
+++ client/src/screens/chat_screen.cpp
@@ -1,30 +1,85 @@
 #include "chat_screen.h"
 
+#include "utils/command/command_parser.h"
 #include "utils/console/console.h"
 
 namespace screen
 {
-    ChatScreen::ChatScreen(AppConfig& cfg, ChatInfo& chat) : config_(cfg), chat_(chat)
-    { }
+    ChatScreen::ChatScreen(app::AppController& controller, ChatInfo& chat) 
+        : controller_(controller), chat_(chat) {}
 
     void ChatScreen::run()
     {
-        printScreen();
+        bool in_chat = true;
+        while (in_chat)
+        {
+            printScreen();
+            displayMessages();
 
-        // todo initialize HttpMessageApi for current server config
-        // todo call fetchMessages(config_.user.id, chat_.peer_id, chat_.last_message_id) to preload chat history
-
-        while (true) {
-            // todo read message input in infinite mode
-            // todo if input starts with "/", call ParseCommand
-            // todo else call HttpMessageApi::sendMessage(config_.user.id, chat_.peer_id, input)
+            const std::string text = io::ScanString("> ");
+            if (text.starts_with("/")) {
+                std::optional<utils::COMMAND> command = utils::ParseCommand(text);
+                if (command == utils::COMMAND::QUIT) {
+                    in_chat = false;
+                }
+                if (command == utils::COMMAND::HELP) {
+                    //todo help output
+                }
+                if (command == std::nullopt) {
+                    io::print("[Error]: unknown command.", io::COLOR::RED);
+                }
+            } else {
+                sendMessage(text);
+            }
         }
     }
 
     void ChatScreen::printScreen()
     {
-        io::print("Chat Screen");
-        io::print("Peer: " + chat_.peer_nick);
-        io::print("Peer ID: " + std::to_string(chat_.peer_id));
+        io::print("\n--- Chat with " + chat_.peer_nick + " (ID: " + std::to_string(chat_.peer_id) + ") ---", io::COLOR::BLUE);
+    }
+
+    void ChatScreen::displayMessages() const {
+        UserInfo other_user;
+        other_user.id = chat_.peer_id;
+        other_user.nickname = chat_.peer_nick;
+
+        auto messages = controller_.GetMessages(other_user, chat_);
+        
+        if (messages.empty())
+        {
+            io::print("(No messages yet)", io::COLOR::YELLOW);
+        }
+        else
+        {
+            for (const auto& msg : messages)
+            {
+                std::string time_str = msg.created_at.empty() ? "" : "[" + msg.created_at + "] ";
+                std::string prefix = (msg.from_id == controller_.GetAppConfig().user.id) ? "[You]: " : "[" + chat_.peer_nick + "]: ";
+                
+                io::print(time_str + prefix + msg.text);
+                
+                if (msg.id > chat_.last_message_id)
+                {
+                    chat_.last_message_id = static_cast<int>(msg.id);
+                }
+            }
+        }
+    }
+
+    void ChatScreen::sendMessage(const std::string& text) const {
+        
+        UserInfo other_user;
+        other_user.id = chat_.peer_id;
+        other_user.nickname = chat_.peer_nick;
+
+        if (controller_.SendMessage(other_user, text))
+        {
+            io::print("Message sent!", io::COLOR::GREEN);
+        }
+        else
+        {
+            io::print("Failed to send message", io::COLOR::RED);
+        }
     }
 }
--- client/src/screens/chat_screen.h
+++ client/src/screens/chat_screen.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "i_screen.h"
-#include "models/app_config.h"
+#include "app/app_controller.h"
 #include "models/chat_info.h"
 
 namespace screen
@@ -9,12 +9,15 @@ namespace screen
     class ChatScreen final : public IScreen
     {
     public:
-        ChatScreen(AppConfig& cfg, ChatInfo& chat);
+        ChatScreen(app::AppController& controller, ChatInfo& chat);
         void run() override;
         void printScreen() override;
 
     private:
-        AppConfig& config_;
+        void displayMessages() const;
+        void sendMessage(const std::string& text) const;
+
+        app::AppController& controller_;
         ChatInfo& chat_;
     };
 }
