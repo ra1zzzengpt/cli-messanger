f25e7e161997ba9c9881e4ae034a4d8c4b20edbd
ra1zzzengpt <ra1zzzengpt@gmail.com>
2026-05-08 11:24:21 +0300

    full http support

--- client/src/api/message_api/httpapi/http_message_api.cpp
+++ client/src/api/message_api/httpapi/http_message_api.cpp
@@ -1,5 +1,187 @@
 #include "http_message_api.h"
 
-namespace api {
+#include <curl/curl.h>
 
+#include "http_response.h"
+
+namespace api {
+    namespace
+    {
+        size_t WriteCallback (const char* ptr, const size_t size, const size_t nmemb, void* userdata) {
+            const size_t real_size = size * nmemb;
+            if (auto* buffer = static_cast<std::string*>(userdata)) {
+                buffer->append(ptr, real_size);
+                return real_size;
+            }
+            return 0;
+        }
+
+        void ParseResponse (const std::string& buffer, HttpResponse& response) {
+            try {
+                if (!buffer.empty()) {
+                    response.data = nlohmann::json::parse(buffer);
+                }
+            } catch (const nlohmann::json::parse_error& e) {
+                response.error_details = "JSON parse error: " + std::string(e.what());
+            }
+        }
+
+        HttpResponse GET (const std::string& url) {
+            HttpResponse response;
+            CURL* handle = curl_easy_init();
+            if (!handle)
+            {
+                response.error_details = "curl_easy_init failed";
+                return response;
+            }
+
+            std::string buffer;
+            curl_easy_setopt(handle, CURLOPT_URL, url.c_str());
+            curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, WriteCallback);
+            curl_easy_setopt(handle, CURLOPT_WRITEDATA, &buffer);
+            curl_easy_setopt(handle, CURLOPT_TIMEOUT, 5L);
+
+            if (const CURLcode res = curl_easy_perform(handle); res != CURLE_OK) {
+                response.error_details = curl_easy_strerror(res);
+            } else {
+                curl_easy_getinfo(handle, CURLINFO_RESPONSE_CODE, &response.status_code);
+                ParseResponse(buffer, response);
+            }
+
+            curl_easy_cleanup(handle);
+            return response;
+        }
+
+        HttpResponse POST (const std::string& url, const std::string& json_body) {
+            HttpResponse response;
+            CURL* handle = curl_easy_init();
+            if (!handle)
+            {
+                response.error_details = "curl_easy_init failed";
+                return response;
+            }
+
+            std::string buffer;
+            curl_slist* headers = nullptr;
+            headers = curl_slist_append(headers, "Content-Type: application/json");
+
+            curl_easy_setopt(handle, CURLOPT_URL, url.c_str());
+            curl_easy_setopt(handle, CURLOPT_POSTFIELDS, json_body.c_str());
+            curl_easy_setopt(handle, CURLOPT_HTTPHEADER, headers);
+            curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, WriteCallback);
+            curl_easy_setopt(handle, CURLOPT_WRITEDATA, &buffer);
+            curl_easy_setopt(handle, CURLOPT_TIMEOUT, 5L);
+
+            if (const CURLcode res = curl_easy_perform(handle); res != CURLE_OK) {
+                response.error_details = curl_easy_strerror(res);
+            } else {
+                curl_easy_getinfo(handle, CURLINFO_RESPONSE_CODE, &response.status_code);
+                ParseResponse(buffer, response);
+            }
+
+            curl_slist_free_all(headers);
+            curl_easy_cleanup(handle);
+            return response;
+        }
+
+        HttpResponse PATCH(const std::string& url, const std::string& json_body) {
+            HttpResponse response;
+            CURL* handle = curl_easy_init();
+            if (!handle)
+            {
+                response.error_details = "curl_easy_init failed";
+                return response;
+            }
+
+            std::string buffer;
+            curl_slist* headers = nullptr;
+            headers = curl_slist_append(headers, "Content-Type: application/json");
+
+            curl_easy_setopt(handle, CURLOPT_URL, url.c_str());
+            curl_easy_setopt(handle, CURLOPT_CUSTOMREQUEST, "PATCH");
+            curl_easy_setopt(handle, CURLOPT_POSTFIELDS, json_body.c_str());
+            curl_easy_setopt(handle, CURLOPT_HTTPHEADER, headers);
+            curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, WriteCallback);
+            curl_easy_setopt(handle, CURLOPT_WRITEDATA, &buffer);
+            curl_easy_setopt(handle, CURLOPT_TIMEOUT, 5L);
+
+            if (const CURLcode res = curl_easy_perform(handle); res != CURLE_OK) {
+                response.error_details = curl_easy_strerror(res);
+            } else {
+                curl_easy_getinfo(handle, CURLINFO_RESPONSE_CODE, &response.status_code);
+                ParseResponse(buffer, response);
+            }
+
+            curl_slist_free_all(headers);
+            curl_easy_cleanup(handle);
+            return response;
+        }
+    }
+
+    bool HttpMessageApi::registerUser(std::uint64_t id, const std::string& nick) {
+        const std::string url = to_url() + "/users/register";
+
+        nlohmann::json body;
+        body["id"] = std::to_string(id);
+        body["nick"] = nick;
+
+        HttpResponse resp = POST(url, body.dump());
+
+        return resp.is_ok() && resp.data.contains("ok") && resp.data["ok"].get<bool>();
+    }
+
+    std::optional<UserInfo> HttpMessageApi::getUsernameById(const std::uint64_t id) {
+        auto resp = GET(to_url() + "/users/" + std::to_string(id));
+        if (resp.is_ok() && resp.data.value("ok", false)) {
+            UserInfo info;
+            info.id = std::stoull(resp.data["user"]["id"].get<std::string>());
+            info.nickname = resp.data["user"]["nick"].get<std::string>();
+            return info;
+        }
+        return std::nullopt;
+    }
+
+    bool HttpMessageApi::updateNickname(const std::uint64_t id, const std::string& newNick) {
+        nlohmann::json body;
+        body["nick"] = newNick;
+        const HttpResponse resp = PATCH(to_url() + "/users/" + std::to_string(id) + "/nick", body.dump());
+        return resp.is_ok() && resp.data.value("ok", false);
+    }
+
+    bool HttpMessageApi::sendMessage(std::uint64_t fromId, std::uint64_t toId, const std::string& text) {
+        nlohmann::json body;
+        body["from_id"] = std::to_string(fromId);
+        body["to_id"] = std::to_string(toId);
+        body["text"] = text;
+        const HttpResponse resp = POST(to_url() + "/messages/send", body.dump());
+        return resp.is_ok() && resp.data.value("ok", false);
+    }
+
+    std::vector<Message> HttpMessageApi::fetchMessages(const std::uint64_t myId, const std::uint64_t peerId, const uint64_t sinceMessageId) {
+        const std::string url = to_url() + "/messages?me=" + std::to_string(myId) +
+                          "&peer=" + std::to_string(peerId) +
+                          "&since_id=" + std::to_string(sinceMessageId);
+        HttpResponse resp = GET(url);
+
+        std::vector<Message> messages;
+        if (resp.is_ok() && resp.data.value("ok", false)) {
+            for (const auto& item : resp.data["messages"]) {
+                Message msg;
+                msg.id = std::stoull(item["id"].get<std::string>());
+                msg.from_id = std::stoull(item["from_id"].get<std::string>());
+                msg.to_id = std::stoull(item["to_id"].get<std::string>());
+                msg.text = item["text"].get<std::string>();
+                msg.created_at = item["created_at"].get<std::string>();
+                messages.push_back(msg);
+            }
+        }
+        return messages;
+    }
+
+    void HttpMessageApi::set_host(const std::string& host) { host_ = host; }
+    void HttpMessageApi::set_port(const std::string& port) { port_ = port; }
+    std::string HttpMessageApi::to_url() const
+    {
+        return "http://" + host_ + ":" + port_;
+    }
 }
--- client/src/api/message_api/httpapi/http_message_api.h
+++ client/src/api/message_api/httpapi/http_message_api.h
@@ -1,7 +1,42 @@
 #pragma once
+#include "api/message_api/imessage_api.h"
 
 namespace api {
-    class HttpMessageApi {
-        // todo override api
+    class HttpMessageApi : public IMessageApi{
+    public:
+        HttpMessageApi() = default;
+
+        bool registerUser(
+            std::uint64_t id,
+            const std::string& nick
+        ) override;
+
+        std::optional<UserInfo> getUsernameById(
+            std::uint64_t id
+        ) override;
+
+        bool updateNickname(
+            std::uint64_t id,
+            const std::string& newNick
+        ) override;
+
+        bool sendMessage(
+            std::uint64_t fromId,
+            std::uint64_t toId,
+            const std::string& text
+        ) override;
+
+        std::vector<Message> fetchMessages(
+            std::uint64_t myId,
+            std::uint64_t peerId,
+            uint64_t sinceMessageId
+        ) override;
+
+        void set_host(const std::string& host) override;
+        void set_port(const std::string& port) override;
+    private:
+        std::string host_;
+        std::string port_;
+        [[nodiscard]] std::string to_url() const;
     };
 }
