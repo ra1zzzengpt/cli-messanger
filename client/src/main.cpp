c2620798d0a531524c5c332a6bfe0a594ca4dd04
ra1zzzengpt <ra1zzzengpt@gmail.com>
2026-05-08 11:26:04 +0300

    http moved

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,7 +1,11 @@
 #include "app/app_controller.h"
 #include "screens/main_screen.h"
 #include "utils/files/config_storage/config_storage.h"
-#include "api/message_api/fakeapi/fake_message_api.h"
+#include "api/message_api/httpapi/http_message_api.h"
+#include <curl/curl.h>
+
+#include "utils/console/console.h"
+
 namespace
 {
     // YOU CAN CHANGE IT ON YOUR CONFIG FILEPATH
@@ -10,8 +14,12 @@ namespace
 
 int main()
 {
-
-    app::AppController controller(std::make_unique<api::FakeMessageApi>(),std::make_unique<utils::ConfigStorage>(kConfigPath));
+    if (const CURLcode init_code = curl_global_init(CURL_GLOBAL_ALL); init_code != CURLE_OK)
+    {
+        io::print("[Fatal error]: curl init failed: " + std::to_string(init_code));
+        return 1;
+    }
+    app::AppController controller(std::make_unique<api::HttpMessageApi>(),std::make_unique<utils::ConfigStorage>(kConfigPath));
 
     screen::MainScreen mainMenu(controller);
     mainMenu.run();
