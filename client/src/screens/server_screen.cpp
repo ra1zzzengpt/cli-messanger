bee540971ba11418af495adf11f9f1afd3f314a1
ra1zzzengpt <ra1zzzengpt@gmail.com>
2026-05-09 19:36:41 +0300

    fixed an error saving the server configuration

--- client/src/screens/server_screen.cpp
+++ client/src/screens/server_screen.cpp
@@ -4,7 +4,8 @@
 
 #include <string>
 
-#include "utils/files/file_utils.h"
+#include "utils/files/files.h"
+#include "utils/files/paths.h"
 
 namespace screen
 {
@@ -21,10 +22,10 @@ namespace screen
                 case static_cast<uint32_t>(kSERVER_MENU::kChangeIp):
                 {
                     std::string new_host = io::ScanString("Enter new host: ");
+                    controller_.GetAppConfig().server.host = new_host;
+                    controller_.GetMessageApi().set_host(new_host);
                     if (controller_.SaveAppConfig())
                     {
-                        controller_.GetAppConfig().server.host = new_host;
-                        controller_.GetMessageApi().set_host(new_host);
                         io::print("[Success]: new host changed",io::COLOR::GREEN);
                     } else
                     {
@@ -35,10 +36,10 @@ namespace screen
                 case static_cast<uint32_t>(kSERVER_MENU::kChangePort):
                 {
                     std::string new_port = io::ScanString("Enter new port: ");
+                    controller_.GetAppConfig().server.host = new_port;
+                    controller_.GetMessageApi().set_port(new_port);
                     if (controller_.SaveAppConfig())
                     {
-                        controller_.GetAppConfig().server.host = new_port;
-                        controller_.GetMessageApi().set_port(new_port);
                         io::print("[Success]: new port changed.",io::COLOR::GREEN);
                     } else
                     {
@@ -62,12 +63,16 @@ namespace screen
 
     void ServerScreen::printScreen()
     {
-        io::print("Server Screen");
-        io::print("Current server:");
+        utils::PrintFromFile(paths::SERVER);
         io::print("Host: " + controller_.GetAppConfig().server.host);
         io::print("Port: " + std::to_string(controller_.GetAppConfig().server.port));
-        io::print("1 - Change IP");
-        io::print("2 - Change port");
-        io::print("3 - Exit");
+        io::print("Checking server status...");
+        if (const std::optional<std::string> host = controller_.Ping(); host.has_value())
+        {
+            io::print(host.value(), io::COLOR::GREEN);
+        } else
+        {
+            io::print("[Error]: server now is offline", io::COLOR::RED);
+        }
     }
 }
