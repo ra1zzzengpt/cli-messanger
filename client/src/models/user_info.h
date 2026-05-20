757b99116423376af3eb5ae1c390ebdab7646634
ra1zzzengpt <ra1zzzengpt@gmail.com>
2026-05-20 22:19:30 +0300

    fixed to rule of 5

--- client/src/models/user_info.h
+++ client/src/models/user_info.h
@@ -3,12 +3,25 @@
 #include <cstdint>
 #include <string>
 #include <nlohmann/json.hpp>
+#include <sodium/utils.h>
 
 struct UserInfo
 {
     std::uint64_t id = 0;
     std::string nickname;
     std::string password;
+
+    UserInfo() = default;
+    UserInfo(const UserInfo&) = default;
+    UserInfo& operator=(const UserInfo&) = default;
+    UserInfo(UserInfo&&) = default;
+    UserInfo& operator=(UserInfo&&) = default;
+
+    ~UserInfo()
+    {
+        if (!password.empty())
+            sodium_memzero(password.data(), password.size());
+    }
 };
 
 void to_json(nlohmann::json& json, const UserInfo& user);
