91df18b21cc536a46c452114557a37d5a843ac96
devnull <ra1zzzengpt@gmail.com>
2026-05-20 17:23:50 +0300

    deleted error_details field (std::expected works better)

--- client/src/api/message_api/httpsapi/https_response.h
+++ client/src/api/message_api/httpsapi/https_response.h
@@ -1,16 +1,14 @@
 #pragma once
 
-#include <string>
 #include <nlohmann/json.hpp>
 
 namespace api {
     struct HttpResponse {
         long status_code = 0;
         nlohmann::json data;
-        std::string error_details;
 
         [[nodiscard]] bool is_ok() const {
-            return status_code >= 200 && status_code < 300 && error_details.empty();
+            return status_code >= 200 && status_code < 300;
         }
     };
 }
