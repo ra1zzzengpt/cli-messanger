1e8491eb397b385ff0bcab7e0faa4bab5880b987
ra1zzzengpt <ra1zzzengpt@gmail.com>
2026-07-14 23:31:16 +0300

    WIP: screen runner

---
+++ client/src/screens/screen_fabric_runner.cpp
@@ -0,0 +1,8 @@
+//
+// Created by devnull on 14.07.2026.
+//
+
+#include "screen_fabric_runner.hpp"
+
+namespace screen
+{} // screen
---
+++ client/src/screens/screen_fabric_runner.hpp
@@ -0,0 +1,8 @@
+#pragma once
+namespace screen {
+
+class screen_fabric_runner {
+
+};
+
+} // screen
