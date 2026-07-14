86e0e98fa3a5c6edcbeeafeeaf50cb80f38126fb
ra1zzzengpt <ra1zzzengpt@gmail.com>
2026-07-14 23:31:27 +0300

    WIP: entry fabric

--- client/src/screens/entry_fabric.cpp
+++ client/src/screens/entry_fabric.cpp
@@ -1,5 +1,64 @@
-//
-// Created by devnull on 13.07.2026.
-//
+#include <screens/entry_fabric.hpp>
 
-#include "entry_fabric.hpp"
+#include "utils/low_level_utils.hpp"
+
+namespace screen
+{
+    EntryFabric::EntryFabric(app::AppController &controller) : controller_(controller){ }
+
+    ftxui::Component EntryFabric::createScreen(int& tab_index, ftxui::ScreenInteractive screen)
+    {
+        // CLI - MESSANGER
+        // separator
+        // url input
+        // button check and in!
+
+        ftxui::InputOption url_input_option;
+        url_input_option.content = &url_;
+        url_input_option.multiline = false;
+        url_input_option.placeholder = "url for cli-messanger server...";
+
+        ftxui::Component url_input = ftxui::Input(url_input_option);
+
+        ftxui::ButtonOption url_button_option;
+        url_button_option.label = "next";
+        url_button_option.on_click = [&]
+        {
+            if (stx::checkNoError(controller_.ping(),error_))
+            {
+                if (stx::checkNoError(controller_.updateUrl(url_),error_))
+                {
+                    tab_index = to_int(kScreen::kAuth);
+                }
+            }
+        };
+
+        ftxui::Component url_button = ftxui::Button(url_button_option);
+
+        ftxui::ButtonOption exit_button_option;
+        exit_button_option.label = "exit";
+        exit_button_option.on_click = [&]
+        {
+            screen.ExitLoopClosure();
+        };
+
+        ftxui::Component exit_button = ftxui::Button(exit_button_option);
+
+        const ftxui::Component entry_container = ftxui::Container::Vertical({url_input, url_button, exit_button});
+
+        ftxui::Component entry_renderer = ftxui::Renderer(entry_container, [&]
+        {
+            std::vector<ftxui::Element> elements;
+            elements.push_back(ftxui::text("CLI - MESSANGER"));
+            elements.push_back(ftxui::separator());
+            elements.push_back(entry_container->Render());
+            if (!error_.message.empty())
+            {
+                elements.push_back(ftxui::text(error_.message));
+            }
+            return ftxui::center(ftxui::vbox(elements) | ftxui::border);
+        });
+
+        return entry_renderer;
+    }
+}
--- client/src/screens/entry_fabric.hpp
+++ client/src/screens/entry_fabric.hpp
@@ -1,6 +1,23 @@
 #pragma once
+#include <screens/i_fabric.hpp>
+#include <app/app_controller.hpp>
 
+namespace screen
+{
+    class EntryFabric : public IFabric
+    {
+    public:
+        explicit  EntryFabric(app::AppController& controller);
+        ~EntryFabric() override = default;
+        EntryFabric(const EntryFabric&) = delete;
+        EntryFabric& operator=(const EntryFabric&) = delete;
+        EntryFabric(EntryFabric&&) = delete;
+        EntryFabric& operator=(EntryFabric&&) = delete;
 
-class entry_fabric {
-
-};
+        ftxui::Component createScreen(int &tab_index, ftxui::ScreenInteractive screen) override;
+    private:
+        app::AppController& controller_;
+        stx::err::Error error_;
+        std::string url_;
+    };
+}
