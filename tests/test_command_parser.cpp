14fbe499f7d9629884d4dc3730df4dc3e9f4a355
ra1zzzengpt <ra1zzzengpt@gmail.com>
2026-07-25 19:17:20 +0300

    Fix: help text added

--- tests/test_command_parser.cpp
+++ tests/test_command_parser.cpp
@@ -5,6 +5,7 @@ using stx::parseCommand;
 using stx::Command;
 
 TEST(CommandParser, KnownCommands) {
+    EXPECT_EQ(parseCommand("/help"),   Command::Help);
     EXPECT_EQ(parseCommand("/quit"),   Command::Quit);
     EXPECT_EQ(parseCommand("/update"), Command::Update);
     EXPECT_EQ(parseCommand("/dump"),   Command::Dump);
